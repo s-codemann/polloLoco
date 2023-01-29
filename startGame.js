760bfcb27beac5452bb2728a83ecdbf40f0f4919
s.codemann <s.codemann@protonmail.com>
2023-01-29 23:28:54 +0100

    instructions added

--- startGame.js
+++ startGame.js
@@ -8,13 +8,21 @@ function startScreen() {
   startBtn.addEventListener("click", initGame);
   startDiv.classList.add("start");
   startDiv.style.top = canvas.getBoundingClientRect().top;
-  document.body.append(startDiv);
   setTimeout(() => startDiv.append(startBtn), 1000);
   startScreenEl = startDiv;
+  let instructionDiv = document.createElement("div");
+  instructionDiv.innerHTML = ` <ul class="instructions">
+      <li class="instruction">f: throw bottle</li>
+      <li class="instruction">spacebar: jump</li>
+      <li class="instruction">left-arrow: move left</li>
+      <li class="instruction">right-arrow: move right</li>
+    </ul>"`;
+  startDiv.append(instructionDiv);
+  //document.body.append(startDiv);
+  document.body.insertBefore(startDiv, document.getElementById("game"));
 }
 function initGame() {
   world.gameStarted = true;
-  document.body.removeChild(startScreenEl);
-
+  document.body.removeChild(document.querySelector("div"));
   restartGame();
 }
